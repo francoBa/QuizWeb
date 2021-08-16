3caadbbd9c2460ad2c8602d85a730dc7774fc038
GACB-Ar <gerocb.lab@gmail.com>
2021-08-16 18:18:23 -0300

    added columns id_pk and category. modified display for DB in django administrator

--- core/models.py
+++ core/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class QuesModel(models.Model):
+    id_pk = models.AutoField(primary_key=True)
+    category = models.CharField(max_length=30, null=True)
     question = models.CharField(max_length=200,null=True)
     op1 = models.CharField(max_length=200,null=True)
     op2 = models.CharField(max_length=200,null=True)
@@ -10,4 +12,5 @@ class QuesModel(models.Model):
     ans = models.CharField(max_length=200,null=True)
     
     def __str__(self):
+        ques_display = f"ID: {self.id_pk} | Category: {self.category} | Question: {self.question}"
         return self.question
