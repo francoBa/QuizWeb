7c760bb7199bbc524c6e0f50aad2b42b17d64920
GACB-Ar <gerocb.lab@gmail.com>
2021-08-17 13:39:01 -0300

    op4 has been erased from quesModel

--- core/models.py
+++ core/models.py
@@ -8,7 +8,6 @@ class QuesModel(models.Model):
     op1 = models.CharField(max_length=200,null=True)
     op2 = models.CharField(max_length=200,null=True)
     op3 = models.CharField(max_length=200,null=True)
-    op4 = models.CharField(max_length=200,null=True)
     ans = models.CharField(max_length=200,null=True)
     
     def __str__(self):
