b4f726686870db20ee7a52bbe664588c41f972e6
francobarreto <franco.barreto@gmail.com>
2021-08-26 12:09:33 -0300

    se actualizan los forms.py y los templates para que se acomoden los estilos

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,22 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
-from django.db import models
-from django.db.models import fields
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm, AuthenticationForm
+from django import forms
 from .models import Perfil
+
+
+class IniciarSesionForm(AuthenticationForm):
+  class Meta:
+    model = Perfil
+    fields = (
+      "username",
+      "password"
+    )
+  
+  def __init__(self, *args, **kwargs):
+    super(IniciarSesionForm, self).__init__(*args, **kwargs)
+    self.fields['username'].widget = forms.TextInput(
+        attrs={'class': 'form-input', 'placeholder': 'ingrese su usuario'})
+    self.fields['password'].widget = forms.PasswordInput(
+        attrs={'class': 'form-input', 'placeholder': 'ingrese su contraseña'})
 
 
 class NuevoPerfilForm(UserCreationForm):
@@ -16,6 +31,20 @@ class NuevoPerfilForm(UserCreationForm):
       "password2",
     )
 
+    widgets = {
+      'first_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'ingrese su nombre'}),
+      'last_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'ingrese su apellido'}),
+      'username': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'nombre de usuario'}),
+      'email': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'ingrese su email'})
+    }
+
+  def __init__(self, *args, **kwargs):
+    super(NuevoPerfilForm, self).__init__(*args, **kwargs)
+    self.fields['password1'].widget = forms.PasswordInput(
+        attrs={'class': 'form-input', 'placeholder': 'ingrese su contraseña'})
+    self.fields['password2'].widget = forms.PasswordInput(
+        attrs={'class': 'form-input', 'placeholder': 'confirme su contraseña'})
+
 
 class EditarPerfilForm(UserChangeForm):
   class Meta:
@@ -27,6 +56,13 @@ class EditarPerfilForm(UserChangeForm):
       "email",
     )
 
+    widgets = {
+      'first_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'ingrese su nombre'}),
+      'last_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'ingrese su apellido'}),
+      'username': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'nombre de usuario'}),
+      'email': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'ingrese su email'})
+    }
+
 
 class CambiarPassword(PasswordChangeForm):
   class Meta:
@@ -36,3 +72,12 @@ class CambiarPassword(PasswordChangeForm):
       "new_password1",
       "new_password2",
     )
+
+  def __init__(self, *args, **kwargs):
+    super(CambiarPassword, self).__init__(*args, **kwargs)
+    self.fields['old_password'].widget = forms.PasswordInput(
+        attrs={'class': 'form-input', 'placeholder': 'contraseña anterior'})
+    self.fields['new_password1'].widget = forms.PasswordInput(
+        attrs={'class': 'form-input', 'placeholder': 'nueva contraseña'})
+    self.fields['new_password2'].widget = forms.PasswordInput(
+        attrs={'class': 'form-input', 'placeholder': 'confirme su contraseña'})
